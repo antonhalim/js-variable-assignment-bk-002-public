56bf1cb7ea80d1788ec7fa7aac22bf8227933121
Steven Nunez <steven.nunez@gmail.com>
2014-05-17 11:32:41 -0400

    Add Tests

---
+++ README.md
@@ -0,0 +1,6 @@
+---
+languages: javascript
+tags: variable assignment, variables
+---
+
+# Local Variable Assignment
---
+++ spec/variableAssignment.spec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Variable Assignment', function() {
+  it('proves your greatness', function() {
+    // Define a local variable greeting and give it the value of Hello World
+    expect(greeting).toEqual("Hello World");
+  });
+});
