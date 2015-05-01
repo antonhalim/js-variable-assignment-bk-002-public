058066a77814321e682e9f0daefc9cfe2a1467b5
Katie Hoffman <ktahoffman@gmail.com>
2015-05-01 12:09:50 -0400

    make file in lib to get learn gem to test lab

--- spec/variableAssignment.spec.js
+++ spec/variableAssignment.spec.js
@@ -4,7 +4,7 @@ describe('Variable Assignment', function() {
   it('proves your greatness', function() {
     // Make a local variable "greeting" and give it the value of Hello World
     // v Define variable below v
-    var greeting = "Hello World"
+
     // ^ Define variable above ^
     expect(greeting).toEqual("Hello World");
   });
