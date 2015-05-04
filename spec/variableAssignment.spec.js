302fd0b225dca922cc2e63a69c51ed1884d74a58
axton21 <axton_n@yahoo.com>
2015-05-04 09:29:45 -0400

    done

--- spec/variableAssignment.spec.js
+++ spec/variableAssignment.spec.js
@@ -2,10 +2,8 @@
 
 describe('Variable Assignment', function() {
   it('proves your greatness', function() {
-    // Make a local variable "greeting" and give it the value of Hello World
-    // v Define variable below v
+    var greeting = "Hello World"
 
-    // ^ Define variable above ^
     expect(greeting).toEqual("Hello World");
   });
 });
